d5785ab0680210e0b7115580c9429ad0fc07d89b
shaxi007 <teshayevshaxboz77@gmail.com>
2021-11-24 16:47:32 +0500

    feat: files added

--- server.py
+++ server.py
@@ -7,6 +7,13 @@ import uuid
 app = Flask('app')
 
 UPLOAD_FOLDER = f'{os.path.dirname(os.path.realpath(__file__))}/uploads/'
+ON_HEROKU = os.environ.get('ON_HEROKU')
+port=3000
+if ON_HEROKU:
+    # get the heroku port 
+    port = int(os.environ.get("PORT", 17995))  # as per OP comments default is 17995
+else:
+    port = 3000
 
 
 @app.route('/')
@@ -32,4 +39,4 @@ def download_file(filename):
     return send_file(UPLOAD_FOLDER + 'compressed/' + filename, as_attachment=True)
 
 
-app.run(host='0.0.0.0', port=8080)
+app.run(host='0.0.0.0', port)
